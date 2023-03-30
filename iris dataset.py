f503ba4708760bf60dca8eb67207e13483fc0780
sanskritimagdum8 <129357220+sanskritimagdum8@users.noreply.github.com>
2023-03-30 13:23:14 +0530

    Add files via upload

---
+++ iris dataset.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import csv
+
+df=pd.read_csv(r'C:\Users\admin\Downloads\IRIS.csv')
+df.head()
+
+
+# In[2]:
+
+
+setosa = df[df['species'] == 'Iris-setosa']
+versicolor = df[df['species'] == 'Iris-versicolor']
+virginica = df[df['species'] == 'Iris-virginica']
+
+
+# In[5]:
+
+
+print('Iris-setosa')
+setosa.describe()
+
+
+# In[6]:
+
+
+print('\nIris-versicolor')
+versicolor.describe()
+
+
+# In[7]:
+
+
+print('\nIris-virginica')
+virginica.describe()
+
+
+# In[ ]:
+
+
+
+
